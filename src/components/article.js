b933a9fef2b571b844492553d4e15781c0e575e5
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2021-08-26 17:27:27 +0100

    react app installation

---
+++ src/components/article.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Article(){
+	return(
+
+	)
+}
+
+export default Article
---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	appBar: {
+		borderBottom: `1px solid ${theme.palette.divider}`,
+		backgroundColor: 'hsl(212, 23%, 69%)',
+	},
+	name: {
+		marginLeft: '10px',
+	}
+}));
+
+function Header() {
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<AppBar
+				position="static"
+				color="white"
+				elevation={0}
+				className={classes.appBar}
+			>
+				<Toolbar>
+					<Typography variant="h6" color="inherit" noWrap className={classes.name}>
+						Articles Preview
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</React.Fragment>
+	);
+}
+
+export default Header;
