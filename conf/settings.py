181d20f7e9f0d8420485a996ab7eea362cb62cb6
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2021-09-08 15:51:10 +0100

    access_token extended to 1 day

--- conf/settings.py
+++ conf/settings.py
@@ -148,7 +148,7 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = "users.CustomUser"
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
