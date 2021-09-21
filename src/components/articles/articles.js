22627a670cb0cf75e402d06f8e57e33349bc462a
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2021-09-21 10:34:14 +0100

    author section content in articles.js

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -1,40 +1,20 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import {
+	Card,
+	CardContent,
+	CardMedia,
+	Grid,
+	Typography,
+	Container,
+	Box,
+	Avatar,
+} from '@material-ui/core';
+import { format } from "date-fns";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		// direction: "column",
-		// display: "flex",
-		// alignItems: "center",
-		// flexDirection: "column",
-	},
-	card: {
-		// display: 'flex',
-		// width: '600px',
-		// height: '250px',
-	},
-	details: {
-		display: 'flex',
-		flexDirection: 'column',
-	},
 	cardMedia: {
-		// paddingTop: '56.25%', // 16:9
 		width: '250px',
-	},
-	link: {
-		margin: theme.spacing(1, 1.5),
-	},
-	cardHeader: {
-		backgroundColor:
-			theme.palette.type === 'light'
-				? theme.palette.grey[200]
-				: theme.palette.grey[700],
 	},
 	cardContent: {
 		display: 'flex',
@@ -58,6 +38,13 @@ const useStyles = makeStyles((theme) => ({
 	articleAuthor:{
 		height: '50px',
 		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+	},
+	authorInfo: {
+		alignItems: 'baseline',
+		textAlign: 'left',
+		marginLeft: theme.spacing(1),
 	},
 	[theme.breakpoints.down('xs')]: {
 		root: {
@@ -70,15 +57,13 @@ const useStyles = makeStyles((theme) => ({
 			width: '90%',
 			height: 'auto',
 			margin: 'auto',
-			// backgroundColor: 'red',
 		},
 		cardMedia: {
-			// paddingTop: '56.25%', // 16:9
 			width: '100%',
 			height: '300px',
 		},
 		articleAuthor:{
-			backgroundColor: 'blue',
+			height: 'auto',
 		},		
 	},
 	[theme.breakpoints.up('sm')]: {
@@ -92,18 +77,13 @@ const useStyles = makeStyles((theme) => ({
 			width: '90%',
 			height: 'auto',
 			margin: 'auto',
-			// backgroundColor: 'red',
 		},
 		cardMedia: {
-			// paddingTop: '56.25%', // 16:9
 			width: '40%',
 			height: 'auto',
 		},
 		cardContent: {
 			width: '60%',
-		},
-		articleAuthor:{
-			backgroundColor: 'red',
 		},
 	},
 	[theme.breakpoints.up('md')]: {
@@ -117,19 +97,14 @@ const useStyles = makeStyles((theme) => ({
 			width: '90%',
 			height: '550px',
 			margin: 'auto',
-			// backgroundColor: 'red',
 		},
 		cardMedia: {
-			// paddingTop: '56.25%', // 16:9
 			width: '100%',
 			height: '55%',
 		},
 		cardContent: {
 			height: '45%',
 			width: '100%',
-		},
-		articleAuthor:{
-			backgroundColor: 'yellow',
 		},
 	},
 	[theme.breakpoints.up('lg')]: {
@@ -143,10 +118,8 @@ const useStyles = makeStyles((theme) => ({
 			width: '90%',
 			height: '270px',
 			margin: 'auto',
-			// backgroundColor: 'red',
 		},
 		cardMedia: {
-			// paddingTop: '56.25%', // 16:9
 			width: '40%',
 			height: 'auto',
 		},
@@ -154,16 +127,16 @@ const useStyles = makeStyles((theme) => ({
 			width: '60%',
 			height: '100%',
 		},
-		articleAuthor:{
-			backgroundColor: 'green',
-		},
 	},
 }));
 
 const Articles = (props) => {
-	const { articles } = props;
 	const classes = useStyles();
-	console.log("props in articles", props)
+	const { articles, authors } = props;
+	const formatCreated = (date) =>{
+		var articleCreated = new Date(date);
+		return format(articleCreated, "dd MMM yyyy");
+	}
 	if (!articles || articles.length === 0) return <p>Can not find any articles, sorry</p>;
 	return (
 		<React.Fragment>
@@ -172,8 +145,7 @@ const Articles = (props) => {
 					container 
 					spacing={3} 
 					className={classes.root}
-					// alignItems="center"
-					>
+				>
 					{articles.map((article) => {
 						var articleTitle = article.title.substr(0, 106) + '...'
 						if(article.title.length < 107){
@@ -191,8 +163,9 @@ const Articles = (props) => {
 						if(article.content.length < contentLength){
 							articleText = article.content
 						}
+						const author = authors.find((author) => author.id === article.author)
+						console.log(author)
 						return (
-							// Enterprise card is full width at sm breakpoint
 							<Grid item key={article.id} xs={12} sm={12} md={6} lg={6}>
 								<Card className={classes.card}>
 									<CardMedia
@@ -219,7 +192,22 @@ const Articles = (props) => {
 											</Typography>
 										</div>
 										<div className={classes.articleAuthor}>
-											author section
+											<Avatar
+												src={author.avatar}
+											/>
+											<div className={classes.authorInfo}>
+												<Typography>
+													<Box fontWeight="bold">
+														{author.first_name} {author.last_name}
+													</Box>
+												</Typography>
+												<Typography
+													variant="p" 
+													color="textSecondary"
+												>
+													{formatCreated(article.created)}
+												</Typography>
+											</div>
 										</div>
 									</CardContent>
 								</Card>
