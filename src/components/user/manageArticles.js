5d40bc6253774dd6eddd6f242455e80715343baa
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2022-06-07 17:36:18 +0100

    marginBottom fixed in manageArticles.js try1

--- src/components/user/manageArticles.js
+++ src/components/user/manageArticles.js
@@ -18,6 +18,7 @@ import {useUserContext} from '../context/UserContexProvider'
 const useStyles = makeStyles((theme) => ({
 	root: {
 		marginTop: '100px',
+		marginBottom: '50px',
 	},
 	addButton: {
 		[theme.breakpoints.down('xs')]: {
