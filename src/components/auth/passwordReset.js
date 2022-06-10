d2f42f1c566adb07afe1736718c963f6cee7dfba
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2022-06-10 17:35:35 +0100

    password reset email trim and lowercase

--- src/components/auth/passwordReset.js
+++ src/components/auth/passwordReset.js
@@ -59,7 +59,7 @@ export default function PasswordReset() {
         setUserState({isSigningUp:true})
     }, [])
     const handleChange = (e) => {
-        const email = e.target.value
+        const email = e.target.value.trim().toLowerCase()
         setEmail(email)
         // check if email is valid
         var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
